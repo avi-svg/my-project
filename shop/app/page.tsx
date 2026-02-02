bcd7985217b6446612217ba3d450652388171522
avram <avramy.kunin@gmail.com>
2026-02-02 18:59:11 +0200

    Refactor Home component; simplify authentication logic and directly redirect to products

--- shop/app/page.tsx
+++ shop/app/page.tsx
@@ -1,32 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useRouter } from "next/navigation";
-import { refresh } from "./login/services/api";
-import {
-  setAuthenticated,
-  setUnauthenticated,
-} from "./features/auth/authSlice";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkAuthAndRedirect = async () => {
-      try {
-        await refresh();
-        dispatch(setAuthenticated());
-      } catch {
-        dispatch(setUnauthenticated());
-      } finally {
-        router.push("/products");
-      }
-    };
-
-    checkAuthAndRedirect();
-  }, [dispatch, router]);
-
-  return null; 
+  redirect("/products");
 }
